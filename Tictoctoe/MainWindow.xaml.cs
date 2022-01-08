4573a3032082ce8253e883dd6a76e7224ce54c18
TImkistor <tim.kisslove@gmail.com>
2022-01-08 20:05:45 +0300

    Добавьте файлы проекта.

---
+++ Tictoctoe/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace Tictoctoe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private Membert
+
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        /// Default  constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            NeWGame();
+        }
+    }
+    #endregion
+    private void NeWGame()
+    {
+        throw new NotImplementedException();
+    }
+}
