48319c7963ce620efce70968230f0d00a7fa1c67
TImkistor <tim.kisslove@gmail.com>
2022-01-08 21:39:04 +0300

    ssss

--- Tictoctoe/MainWindow.xaml.cs
+++ Tictoctoe/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Tictoctoe
 {
@@ -8,7 +11,10 @@ namespace Tictoctoe
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region Private Membert
+        #region Private Members
+        private TimType[] mResults;
+        private bool mPlayer1Turn;
+        private bool mGameEnded;
 
         #endregion
         #region Constructor
@@ -21,10 +27,47 @@ namespace Tictoctoe
             InitializeComponent();
             NeWGame();
         }
+
+        private void NeWGame()
+        {
+           mResults = new TimType[9];
+
+            for (var i = 0; i<mResults.Length; i++)
+                mResults[i] = new TimType();
+
+            mPlayer1Turn = true;
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = String.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            mGameEnded = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (mGameEnded)
+            {
+                NeWGame();
+                return;
+            }
+
+            var button = (Button)sender;
+
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var index = column + (row*3);
+
+            if (mResults[index] != TimType.Free)
+                return;
+
+            mResults[index] = mPlayer1Turn ? TimType.Cross : TimType.Nought;
+
+            button.Content = mPlayer1Turn ? "X" : "0";
+        }
     }
     #endregion
-    private void NeWGame()
-    {
-        throw new NotImplementedException();
-    }
 }
---
+++ Tictoctoe/TimType.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Tictoctoe
+{
+    /// <summary>
+    ///  The type of value a cell in the game is currently at
+    /// </summary>
+    public enum TimType
+    {
+        Free,
+        Nought,
+        Cross
+    }
+}
